a6d473b7dddc9bf6d4a0b3bf13575a492f3393b8
TheSecondChance <mesakmegerem@gmail.com>
2024-04-18 13:28:40 +0000

    annotation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Task 3's module.
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    take a float arg and return string reperesentation float
+    '''
+    return str(n)
---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+    task 4 module
+    annotate variables with specifid values
+'''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: boolean = True
+school: str = 'Holberton'
